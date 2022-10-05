04c8341067b201f16685e7d440c91c2929c28566
jroddev <jroddev@gmail.com>
2022-10-05 15:46:22 +1100

    fix instance_type, wasn't using var

--- servers.tf
+++ servers.tf
@@ -1,9 +1,12 @@
 resource "aws_instance" "default" {
   ami                    = var.ami_id
-  instance_type          = "t3.nano"
+  instance_type          = var.instance_type
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
   subnet_id              = aws_subnet.default.id
   user_data              = file("user_data.sh")
+
+  # keypair required to connect if not using public ip
+  # key_name               = "<keyname"
 
   tags = {
     Name = "${var.instance_name}"
